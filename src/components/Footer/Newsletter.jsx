78ea73f1e53194dbde1d429eec68bb18f9784571
ralucasimionas <sim_ralu@yahoo.com>
2023-11-03 14:50:40 +0200

    added footer section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to our newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="e-mail" placeholder="Enter your e-mail" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
